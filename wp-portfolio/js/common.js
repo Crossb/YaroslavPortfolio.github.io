497a458b2484b578d7d5fe9896a7d6a89c0d3156
Yaroslavprof <crosskyiv@gmail.com>
2017-09-25 17:57:55 +0300

    commit#1
    wp-portfolio

---
+++ wp-portfolio/js/common.js
@@ -0,0 +1,114 @@
+	// SideBar
+$(document).ready(function() {
+
+	$("body, .left_side").niceScroll({
+		horizrailenabled : false,
+		"verge" : "500"
+	});
+
+	// Menu
+	$(".btn_mnu").click(function() {
+		$(this).toggleClass("active");
+		$(".left_side").toggleClass("active");
+	});
+
+	$(".gallery img").lazyload({
+		effect : "fadeIn"
+		// затухання, яскравіше
+	}).parent().hover(function() {
+		$(".gallery a").css("opacity", ".8");
+		$(this).css("opacity", "1");
+	}, function(){
+		$(".gallery a").css("opacity", "1");
+	});
+
+
+
+// Preloader
+	$(window).load(function() {
+
+		$(".loader_inner").fadeOut();
+		$(".loader").delay(400).fadeOut("slow");
+
+	});
+
+	// Gallery
+	var wall = new freewall(".gallery");
+	wall.reset({
+		selector: "a",
+		animate: true,
+		cellW: 150,
+		cellH: "auto",
+		gutterX : 5,
+		gutterY : 5,
+		onResize: function() {
+			wall.fitWidth();
+		}
+	});
+
+	var images = wall.container.find("a");
+	images.find("img").load(function() {
+		wall.fitWidth();
+	});
+
+$(".filter_label").click(function() {
+	$(".filter_label").removeClass("active");
+	var filter = $(this).addClass("active").data("filter");
+	wall.filter(filter);
+	setTimeout(function() {
+		$(window).resize();
+		wall.fitWidth();
+	}, 400);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#callback").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				
+			}, 1000);
+		});
+		return false;
+	});
+
+
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
